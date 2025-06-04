fce55fa5791fe560ec6c417d896bd2b54192b6d5
Gustavo Ronan <gustavoronn@gmail.com>
2025-06-04 14:26:26 -0300

    add: mocando lista de dados

--- src/main/java/br/com/gustavoronan/controllers/PersonController.java
+++ src/main/java/br/com/gustavoronan/controllers/PersonController.java
@@ -1,9 +1,11 @@
 package br.com.gustavoronan.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +28,10 @@ public class PersonController implements Serializable{
 	public Person findById (@PathVariable("id")String id) {
 		Person person = service.findById(id);
 		return person;
+	}
+	
+	@GetMapping("/lista")
+	public List<Person> findAll () {
+		return service.findAll();
 	}
 }
--- src/main/java/br/com/gustavoronan/request/PersonServices.java
+++ src/main/java/br/com/gustavoronan/request/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.gustavoronan.request;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -23,6 +25,32 @@ public class PersonServices {
 		person.setAdress("Foz do Iguaçu - Parana - Brasil");
 		person.setGender("Male");
 		
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for(int i = 1; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+	
+	
+
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Nome" + i);
+		person.setLastName("Sobrenome" + i);
+		person.setAdress("Endereço" + i);
+		if(person.getId() % 2 == 0) {
+			person.setGender("Male"+ i);
+		}else {			
+			person.setGender("Female" + i);
+		}
 		return person;
 	}
 	
